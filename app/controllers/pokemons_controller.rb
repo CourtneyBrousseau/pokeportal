389f2b629d435fc1f0ad3d3ada78d0a42b387b95
Courtney Brousseau <courtney.brousseau@berkeley.edu>
2018-10-21 01:00:32 -0700

    Form not working just yet :(

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -18,4 +18,20 @@ class PokemonsController < ApplicationController
 	def new
 		@pokemon = Pokemon.new
 	end
+
+	def create
+    	@pokemon = Pokemon.new
+    	@pokemon.name = params[:name]
+    	@pokemon.ndex = params[:ndex]
+    	@pokemon.trainer = current_trainer
+    	@pokemon.health = 100
+    	@pokemon.level = 1
+    	@pokemon.save
+    	redirect_to trainer_path(current_trainer)
+  	end
+
+  	private
+    def pokemon_params
+    	params.permit(:name, :ndex)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   patch "/capture", to: "pokemons#capture"
   patch "/damage", to: "pokemons#damage"
   get "/pokemons/new", to: "pokemons#new"
+  post "/pokemons/create", to: "pokemons#create"
 end
